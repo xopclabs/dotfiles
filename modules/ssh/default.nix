a226a480f3a0d3fb8b12a5a834791b85fcb67b67
xopclabs <22743369+xopclabs@users.noreply.github.com>
2024-01-12 03:12:12 +0400

    Added ssh keys

--- modules/ssh/default.nix
+++ modules/ssh/default.nix
@@ -6,15 +6,13 @@ let
 in {
     options.modules.ssh = { enable = mkEnableOption "ssh"; };
     config = mkIf cfg.enable {
-        programs.ssh.enable = true;
-        sops.secrets."ssh/id_ed25519" = {
-            path = "/home/xopc/.ssh/id_ed25519";
-        };
-        sops.secrets."ssh/id_ed25519.pub" = {
-            path = "/home/xopc/.ssh/id_ed25519.pub";
-        };
-        sops.secrets."ssh/config" = {
-            path = "/home/xopc/.ssh/config";
+        sops.secrets."ssh/config".path = "/home/xopc/.ssh/hosts_config";
+        sops.secrets."ssh/id_ed25519".path = "/home/xopc/.ssh/id_ed25519";
+        sops.secrets."ssh/id_ed25519.pub".path = "/home/xopc/.ssh/id_ed25519.pub";
+
+        programs.ssh = {
+            enable = true;
+            includes = [ "hosts_config" ];
         };
     };
 }
