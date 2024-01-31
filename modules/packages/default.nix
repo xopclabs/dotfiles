06fc6a944921fc8293b68431f8fe7543df2ff740
xopclabs <github@conv2d.com>
2024-02-01 00:30:53 +0400

    Updated slack to use wayland

--- modules/packages/default.nix
+++ modules/packages/default.nix
@@ -2,6 +2,16 @@
 
 with lib;
 let cfg = config.modules.packages;
+    slack = pkgs.slack.overrideAttrs (old: {
+        installPhase = old.installPhase + ''
+          rm $out/bin/slack
+
+          makeWrapper $out/lib/slack/slack $out/bin/slack \
+            --prefix XDG_DATA_DIRS : $GSETTINGS_SCHEMAS_PATH \
+            --prefix PATH : ${lib.makeBinPath [pkgs.xdg-utils]} \
+            --add-flags "--ozone-platform=wayland --enable-features=UseOzonePlatform,WebRTCPipeWireCapturer"
+        '';
+    });
     maintenance = pkgs.writeShellScriptBin "maintenance" ''${builtins.readFile ../scripts/maintenance}'';
     sftpmpv = pkgs.writeShellScriptBin "sftpmpv" ''${builtins.readFile ../scripts/sftpmpv}'';
     tm = pkgs.writeShellScriptBin "tm" ''${builtins.readFile ../scripts/tm}'';
