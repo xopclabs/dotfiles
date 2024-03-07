a5a5b9871a7753e65781c116bcb112ce42cf041e
xopclabs <22743369+xopclabs@users.noreply.github.com>
2024-03-08 00:21:44 +0400

    Moved to different files

---
+++ modules/hyprland/hypridle.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+let
+    lock = "${pkgs.hyprlock}/bin/hyprlock";
+    systemctl = "${pkgs.systemd}/bin/systemctl";
+    hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
+in 
+{
+    home.file.".config/hypr/hypridle.conf".text = ''
+        listener {
+            timeout = 300                              # 5min
+            on-timeout = ${hyprctl} dispatch dpms off  # screen off when timeout has passed
+            on-resume = ${hyprctl} dispatch dpms on    # screen on when activity is detected after timeout has fired.
+        }
+
+        #listener {
+        #    timeout = 600                              # 10 min
+        #    on-timeout = ${lock}                       # lock screen when timeout has passed
+        #}
+
+        listener {
+            timeout = 1800                             # 30min
+            on-timeout = ${systemctl} suspend          # suspend pc
+        }
+    '';
+}
