bc31384391c9132138fa37655a2e156f0c2daab8
xopclabs <github@conv2d.com>
2024-05-29 14:34:37 +0400

    Working ad2p->hfp switch, woooow

--- hosts/laptop/system/audio.nix
+++ hosts/laptop/system/audio.nix
@@ -2,6 +2,7 @@
 
 {
     # Sound
+    /*
     sound = {
         enable = true;
     };
@@ -10,10 +11,22 @@
         support32Bit = true;
         extraConfig = "load-module module-bluetooth-policy auto_switch=2";
     };
+    */
+    security.rtkit.enable = true;
     services.pipewire = {
-        audio.enable = false;
-        alsa.enable = false;
-        alsa.support32Bit = false;
-        pulse.enable = false;
+        enable = true;
+        audio.enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        jack.enable = true;
+        pulse.enable = true;
+    };
+    services.pipewire.wireplumber.extraConfig = {
+        "monitor.bluez.properties" = {
+            "bluez5.enable-sbc-xq" = true;
+            "bluez5.enable-msbc" = true;
+            "bluez5.enable-hw-volume" = true;
+            "bluez5.roles" = [ "hsp_hs" "hsp_ag" "hfp_hf" "hfp_ag" ];
+        };
     };
 }
