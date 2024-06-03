fad683e0e9d6f2af76f7b64c011168d640993616
xopclabs <github@conv2d.com>
2024-06-03 11:48:59 +0400

    Updated system

--- modules/hyprland/hypridle.nix
+++ modules/hyprland/hypridle.nix
@@ -6,11 +6,11 @@ let
 in 
 {
     home.file.".config/hypr/hypridle.conf".text = ''
-        listener {
-            timeout = 300                              # 5min
-            on-timeout = ${hyprctl} dispatch dpms off  # screen off when timeout has passed
-            on-resume = ${hyprctl} dispatch dpms on    # screen on when activity is detected after timeout has fired.
-        }
+        # listener {
+        #    timeout = 300                              # 5min
+        #    on-timeout = ${hyprctl} dispatch dpms off  # screen off when timeout has passed
+        #    on-resume = ${hyprctl} dispatch dpms on    # screen on when activity is detected after timeout has fired.
+        # }
 
         # listener {
         #    timeout = 300                              # 5 min
