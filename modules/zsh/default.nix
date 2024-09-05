e2c21684c17fc993034499179897356ba4b5afb7
xopclabs <22743369+xopclabs@users.noreply.github.com>
2024-09-05 04:06:45 +0300

    Removed zmk-nix

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -39,7 +39,7 @@ in {
                 ls = "eza --icons=automatic";
                 tree = "eza --tree --icons=automatic";
                 grep = "grep";
-                reconfig = "STARTDIR=$(pwd); cd $NIXOS_CONFIG_DIR; nix flake lock --update-input zmk-nix; sudo nixos-rebuild switch --flake \"$NIXOS_CONFIG_DIR?submodules=1\" --fast; cd $STARTDIR";
+                reconfig = "STARTDIR=$(pwd); cd $NIXOS_CONFIG_DIR; sudo nixos-rebuild switch --flake \"$NIXOS_CONFIG_DIR?submodules=1\" --fast; cd $STARTDIR";
                 visecret = "nix-shell -p sops --run \"sops $NIXOS_CONFIG_DIR/hosts/laptop/secrets.yaml\"";
             };
 
