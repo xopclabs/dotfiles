d25d06ccca542cd523c8b9b0d0165055d01bda77
xopclabs <b9fyg5ei@duck.com>
2025-02-09 19:38:42 +0300

    Added compose

--- modules/hyprland/hypridle.nix
+++ modules/hyprland/hypridle.nix
@@ -6,20 +6,20 @@ let
 in 
 {
     home.file.".config/hypr/hypridle.conf".text = ''
-        listener {
-          timeout = 300                              # 5min
-          on-timeout = ${hyprctl} dispatch dpms off                            # screen off when timeout has passed
-          on-resume = ${hyprctl} dispatch dpms on && brightnessctl set 100%    # screen on when activity is detected after timeout has fired.
-        }
+        # listener {
+        #  timeout = 900                              # 15min
+        #  on-timeout = ${hyprctl} dispatch dpms off                            # screen off when timeout has passed
+        #  on-resume = ${hyprctl} dispatch dpms on && brightnessctl set 100%    # screen on when activity is detected after timeout has fired.
+        # }
 
         # listener {
         #    timeout = 300                              # 5 min
         #    on-timeout = ${lock}                       # lock screen when timeout has passed
         # }
 
-        listener {
-           timeout = 1800                             # 30min
-           on-timeout = ${systemctl} suspend          # suspend pc
-        }
+        # listener {
+        #   timeout = 1800                             # 30min
+        #   on-timeout = ${systemctl} suspend          # suspend pc
+        # }
     '';
 }
