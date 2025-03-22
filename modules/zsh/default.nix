77b30152852e1d74266f8af52dd791371a7f943c
xopclabs <b9fyg5ei@duck.com>
2025-03-22 23:41:19 +0300

    Add p10k support and enhance zsh configuration
    - Introduced p10k option to enable Powerlevel10k theme in zsh configuration.
    - Updated shell aliases and added new plugins for improved functionality.
    - Enhanced history settings for better management and added fzf-tab integration for completion.
    - Refactored plugin management to conditionally include Powerlevel10k based on configuration.

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -5,6 +5,9 @@ let cfg = config.modules.zsh;
 in {
     options.modules.zsh = { 
         enable = mkEnableOption "zsh"; 
+        p10k = {
+            enable = mkEnableOption "p10k";
+        };
         envExtra = mkOption {
             type = types.lines;
             default = "";
@@ -20,55 +23,97 @@ in {
     config = mkIf cfg.enable {
         home.packages = with pkgs; [
             zsh
-            #zsh-powerlevel10k
+            fzf
+            zsh-fzf-tab
+            zoxide
+            eza
         ];
 
         programs.zsh = {
             enable = true;
-            oh-my-zsh = {
-                enable = true;
-                plugins = [ "git"];
-            };
             dotDir = ".config/zsh";
-
-            history = {
-                path = "/home/${config.home.username}/.zsh_history";
-                ignoreAllDups = true;
-            };
 
             envExtra = cfg.envExtra;
             initContent = cfg.initContent;
+
+            history = {
+                path = "${config.home.homeDirectory}/.zsh_history";
+                share = true;
+                ignoreDups = true;
+                ignoreAllDups = true;
+                ignoreSpace = true;
+                findNoDups = true;
+                saveNoDups = true;
+            };
 
             enableCompletion = true;
             autosuggestion.enable = true;
             syntaxHighlighting.enable = true;
 
-            # Set some aliases
             shellAliases = {
                 mkdir = "mkdir -vp";
                 rm = "rm -rifv";
                 mv = "mv -iv";
                 cp = "cp -riv";
                 cat = "bat --paging=never --style=plain";
-                ls = "eza --icons=automatic";
                 tree = "eza --tree --icons=automatic";
                 grep = "grep";
                 reconfig = "STARTDIR=$(pwd); cd $NIXOS_CONFIG_DIR; sudo nixos-rebuild switch --flake \"$NIXOS_CONFIG_DIR?submodules=1\" --fast; cd $STARTDIR";
                 visecret = "sops $NIXOS_CONFIG_DIR/hosts/laptop/secrets.yaml";
             };
 
+            oh-my-zsh = {
+                enable = true;
+                plugins = [ 
+                    "git"
+                    "sudo"
+                    "eza"
+                    "zoxide"
+                    "fzf"
+                ];
+                extraConfig = ''
+                    # Completion
+                    zstyle ':completion:*' matcher-list 'm:{a-z}={A-Za-z}'
+                '' 
+                + ''
+                    # fzf-tab
+                    zstyle ':completion:*' menu no
+                    zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always --icons=always $realpath'
+                    zstyle ':fzf-tab:complete:__zoxide_z:*' fzf-preview 'eza -1 --color=always --icons=always $realpath'
+                    zstyle ':fzf-tab:complete:ls:*' fzf-preview 'eza -1 --color=always --icons=always $realpath'
+                    zstyle ':fzf-tab:complete:eza:*' fzf-preview 'eza -1 --color=always --icons=always $realpath'
+                    zstyle ':fzf-tab:*' switch-group '<' '>'
+                    # Popup in tmux
+                    # zstyle ':fzf-tab:*' fzf-command ftb-tmux-popup
+                '' 
+                + ''
+                    # Icons for eza
+                    zstyle ':omz:plugins:eza' 'icons' yes
+                ''
+                + ''
+                    # Redefine cd to use zoxide
+                    export ZOXIDE_CMD_OVERRIDE="cd"
+                '';
+            };
+
             # Source all plugins, nix-style
             plugins = [
-                # {
-                #     name = "powerlevel10k";
-                #     src = pkgs.zsh-powerlevel10k;
-                #     file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-                # }
-                # {
-                #     name = "powerlevel10k-config";
-                #     src = lib.cleanSource ./p10k;
-                #     file = "p10k.zsh";
-                # }
+                {
+                    name = pkgs.zsh-fzf-tab.pname;
+                    src = pkgs.zsh-fzf-tab.src;
+                    file = "fzf-tab.zsh";
+                }
+            ] ++ [
+                (mkIf cfg.p10k.enable {
+                    name = pkgs.zsh-powerlevel10k.pname;
+                    src = pkgs.zsh-powerlevel10k;
+                    file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+                })
+                (mkIf cfg.p10k.enable {
+                    name = "powerlevel10k-config";
+                    src = lib.cleanSource ./p10k;
+                    file = "p10k.zsh";
+                })
             ];
 
         };
