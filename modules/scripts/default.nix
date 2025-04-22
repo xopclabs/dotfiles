e589092d4ff59b688848c3a2e82788b97be90e4f
xopclabs <b9fyg5ei@duck.com>
2025-04-22 11:32:23 +0000

    Added run-training script

--- modules/scripts/default.nix
+++ modules/scripts/default.nix
@@ -9,6 +9,7 @@ let
     freshman_start = pkgs.writeShellScriptBin "freshman_start" ''${builtins.readFile ../scripts/freshman_start}'';
     see = pkgs.writeShellScriptBin "see" ''${builtins.readFile ../scripts/see}'';
     ssh-tmux = pkgs.writeShellScriptBin "ssh-tmux" ''${builtins.readFile ../scripts/ssh-tmux}'';
+    run-training = pkgs.writeShellScriptBin "run-training" ''${builtins.readFile ../scripts/run-training}'';
 in {
     options.modules.scripts = { enable = mkEnableOption "scripts"; };
     config = mkIf cfg.enable {
@@ -20,6 +21,7 @@ in {
             freshman_start
             see
             ssh-tmux
+            run-training
         ];
     };
 }
