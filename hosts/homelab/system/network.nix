525f22f4978b4b7fb5096d59db2a3a5256fab098
xopclabs <b9fyg5ei@duck.com>
2025-05-04 15:46:16 +0000

    Added btrfs stuff, static ip

--- hosts/homelab/system/network.nix
+++ hosts/homelab/system/network.nix
@@ -1,8 +1,19 @@
 { config, pkgs, inputs, ... }:
 
 {
-    # Wifi
     networking = {
+	# Static ip
+	useDHCP = false;
+        interfaces.ens18 = {
+	    useDHCP = false;
+	    ipv4.addresses = [{
+                address = "192.168.254.10";
+                prefixLength = 24;
+            }];
+	};
+	defaultGateway = "192.168.254.1";
+	nameservers = [ "192.168.254.5" "8.8.8.8" ];
+	
         networkmanager = {
             enable = true;
             dns = "systemd-resolved";
