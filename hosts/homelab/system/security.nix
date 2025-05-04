3ae8cae0e2fadfaf961fdf9ebd8ff7e3914eaf1e
xopclabs <b9fyg5ei@duck.com>
2025-05-04 14:13:57 +0000

    Added laptop ssh keys

--- hosts/homelab/system/security.nix
+++ hosts/homelab/system/security.nix
@@ -27,9 +27,17 @@
     };
     services.gnome.gnome-keyring.enable = true;
 
+    sops.secrets."ssh-laptop/id_ed25519" = {
+        path = "/home/homelab/.ssh/id_ed25519_laptop";
+	owner = config.users.users.homelab.name;
+    };
+    sops.secrets."ssh-laptop/id_ed25519.pub" = {
+        path = "/home/homelab/.ssh/id_ed25519_laptop.pub";
+	owner = config.users.users.homelab.name;
+    };
     sops = {
         defaultSopsFile = ../secrets.yaml;
-        age.sshKeyPaths = [ "/home/homelab/.ssh/id_ed25519" ];
+        age.sshKeyPaths = [ "/home/homelab/.ssh/id_ed25519" config.sops.secrets."ssh-laptop/id_ed25519".path ];
         age.keyFile = "/home/homelab/.config/sops/age/keys.txt";
     };
 }
