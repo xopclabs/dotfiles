882d4ced759d5ad136f96887ac6178cb5d78cb0d
xopclabs <b9fyg5ei@duck.com>
2025-09-10 02:13:58 +0300

    Modular kanshi

--- modules/desktop/wm/hyprland/kanshi.nix
+++ modules/desktop/wm/hyprland/kanshi.nix
@@ -1,10 +1,99 @@
 { config, pkgs, ... }:
 
 let
-    laptop_internal = "BOE 0x06B7";
-    deck_internal = "Valve Corporation ANX7530 U 0x00000001";
-    monitor_external1 = "HDMI-A-2";
-    monitor_external2 = "DP-1";
+    cfg = config.hardware;
+
+    # Get monitor names from configuration
+    internalMonitor = cfg.monitors.internal or null;
+    externalMonitor = cfg.monitors.external or null;
+
+    # Helper function to generate workspace move commands
+    generateWorkspaceMoves = monitorName: [
+        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 1 ${monitorName}"
+        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 2 ${monitorName}"
+        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 3 ${monitorName}"
+        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 4 ${monitorName}"
+    ];
+
+    # Helper function to generate monitor disable commands
+    generateMonitorDisables = monitorNames: map (name:
+        "${pkgs.hyprland}/bin/hyprctl keyword monitor \"${name}, disable\""
+    ) monitorNames;
+
+    # Wallpaper command
+    wallpaperCmd = "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png";
+
+    # Define kanshi profiles using monitor names from configuration
+    kanshiProfiles = [
+        {
+            profile = {
+                name = "hdmi";
+                outputs = [
+                    {
+                        criteria = "HDMI-A-2";
+                        status = "enable";
+                        mode = externalMonitor.mode;
+                        position = "0,0";
+                        scale = externalMonitor.scale;
+                    }
+                    {
+                        criteria = internalMonitor.name;
+                        status = "enable";
+                        mode = internalMonitor.mode;
+                        scale = internalMonitor.scale;
+                        position = "0,1080";
+                        transform = internalMonitor.transform;
+                    }
+                ];
+                exec = builtins.concatStringsSep ", " ([
+                    "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
+                ] ++ generateWorkspaceMoves "HDMI-A-2");
+            };
+        }
+        {
+            profile = {
+                name = "type-c";
+                outputs = [
+                    {
+                        criteria = "DP-1";
+                        status = "enable";
+                        mode = externalMonitor.mode;
+                        position = "0,0";
+                        scale = externalMonitor.scale;
+                    }
+                    {
+                        criteria = internalMonitor.name;
+                        status = "enable";
+                        mode = internalMonitor.mode;
+                        scale = internalMonitor.scale;
+                        position = "0,1080";
+                        transform = internalMonitor.transform;
+                    }
+                ];
+                exec = builtins.concatStringsSep ", " ([
+                    "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
+                ] ++ generateWorkspaceMoves "DP-1");
+            };
+        }
+        {
+            profile = {
+                name = "on-the-go";
+                outputs = [
+                    {
+                        criteria = internalMonitor.name;
+                        status = "enable";
+                        mode = internalMonitor.mode;
+                        scale = internalMonitor.scale;
+                        position = "0,0";
+                        transform = internalMonitor.transform;
+                    }
+                ];
+                exec = builtins.concatStringsSep ", " ([
+                    "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
+                ] ++ generateMonitorDisables ["HDMI-A-2" "DP-1"]);
+            };
+        }
+    ];
 in {
     home.packages = [ pkgs.swww ];
 
@@ -16,129 +105,6 @@ in {
 
     services.kanshi = {
         enable = true;
-        settings = [
-            {
-                profile = {
-                    name = "laptop-hdmi";
-                    outputs = [
-                        {
-                            criteria = monitor_external1;
-                            status = "enable";
-                            mode = "1920x1080@74.97";
-                            position = "0,0";
-                            scale = 1.0;
-                        }
-                        {
-                            criteria = laptop_internal;
-                            status = "enable";
-                            mode = "1920x1080@60";
-                            scale = 1.0;
-                            position = "0,1080";
-                        }
-                    ];
-                    exec = builtins.concatStringsSep ", " [
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 1 ${monitor_external1}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 2 ${monitor_external1}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 3 ${monitor_external1}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 4 ${monitor_external1}"
-                        "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
-                    ];
-                };
-            }
-            {
-                profile = {
-                    name = "laptop-type-c";
-                    outputs = [
-                        {
-                            criteria = monitor_external2;
-                            status = "enable";
-                            mode = "1920x1080@74.97";
-                            position = "0,0";
-                            scale = 1.0;
-                        }
-                        {
-                            criteria = laptop_internal;
-                            status = "enable";
-                            mode = "1920x1080@60";
-                            scale = 1.0;
-                            position = "0,1080";
-                        }
-                    ];
-                    exec = builtins.concatStringsSep ", " [
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 1 ${monitor_external2}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 2 ${monitor_external2}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 3 ${monitor_external2}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 4 ${monitor_external2}"
-                        "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
-                    ];
-                };
-            }
-            {
-                profile = {
-                    name = "deck-type-c";
-                    outputs = [
-                        {
-                            criteria = monitor_external2;
-                            status = "enable";
-                            mode = "1920x1080@74.97";
-                            position = "0,0";
-                            scale = 1.0;
-                        }
-                        {
-                            criteria = deck_internal;
-                            status = "enable";
-                            mode = "800x1280@90";
-                            scale = 1.0;
-                            position = "0,1080";
-			    transform = "270";
-                        }
-                    ];
-                    exec = builtins.concatStringsSep ", " [
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 1 ${monitor_external2}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 2 ${monitor_external2}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 3 ${monitor_external2}"
-                        "${pkgs.hyprland}/bin/hyprctl dispatch moveworkspacetomonitor 4 ${monitor_external2}"
-                        "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
-                    ];
-                };
-            }
-            {
-                profile = {
-                    name = "on-the-go-laptop";
-                    outputs = [
-                        {
-                            criteria = laptop_internal;
-                            status = "enable";
-                            mode = "1920x1080@60";
-                            scale = 1.0;
-                        }
-                    ];
-                    exec = builtins.concatStringsSep ", " [
-                        "${pkgs.hyprland}/bin/hyprctl keyword monitor \"${monitor_external1}, disable\""
-                        "${pkgs.hyprland}/bin/hyprctl keyword monitor \"${monitor_external2}, disable\""
-                        "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
-                    ];
-                };
-            }
-            {
-                profile = {
-                    name = "on-the-go-deck";
-                    outputs = [
-                        {
-                            criteria = deck_internal;
-                            status = "enable";
-                            mode = "800x1280@90";
-                            scale = 1.0;
-                            transform = "270";
-                        }
-                    ];
-                    exec = builtins.concatStringsSep ", " [
-                        "${pkgs.hyprland}/bin/hyprctl keyword monitor \"${monitor_external1}, disable\""
-                        "${pkgs.hyprland}/bin/hyprctl keyword monitor \"${monitor_external2}, disable\""
-                        "${pkgs.swww}/bin/swww img ~/.config/hypr/wallpaper/nord.png"
-                    ];
-                };
-            }
-        ];
+        settings = kanshiProfiles;
     };
 }
