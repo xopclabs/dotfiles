420c368680f0d7538b6cf9376900e2b236ffcf9b
xopclabs <b9fyg5ei@duck.com>
2025-10-25 18:21:31 +0300

    Updated to 1.21.10

--- hosts/homelab/nixos/services/minecraft.nix
+++ hosts/homelab/nixos/services/minecraft.nix
@@ -13,8 +13,8 @@
 
         servers.distant-horizons = {
             enable = true;
-            autoStart = false;
-            package = pkgs.fabricServers.fabric-1_21_8;
+            autoStart = true;
+            package = pkgs.fabricServers.fabric-1_21_10;
 
             jvmOpts = "-Xms4G -Xmx8G -XX:+UnlockExperimentalVMOptions -XX:+UnlockDiagnosticVMOptions -XX:+AlwaysActAsServerClassMachine -XX:+AlwaysPreTouch -XX:+DisableExplicitGC -XX:+UseNUMA -XX:NmethodSweepActivity=1 -XX:ReservedCodeCacheSize=400M -XX:NonNMethodCodeHeapSize=12M -XX:ProfiledCodeHeapSize=194M -XX:NonProfiledCodeHeapSize=194M -XX:-DontCompileHugeMethods -XX:MaxNodeLimit=240000 -XX:NodeLimitFudgeFactor=8000 -XX:+UseVectorCmov -XX:+PerfDisableSharedMem -XX:+UseFastUnorderedTimeStamps -XX:+UseCriticalJavaThreadPriority -XX:ThreadPriorityPolicy=1 -XX:+UseG1GC -XX:MaxGCPauseMillis=130 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1NewSizePercent=28 -XX:G1HeapRegionSize=16M -XX:G1ReservePercent=20 -XX:G1MixedGCCountTarget=3 -XX:InitiatingHeapOccupancyPercent=10 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=0 -XX:SurvivorRatio=32 -XX:MaxTenuringThreshold=1 -XX:G1SATBBufferEnqueueingThresholdPercent=30 -XX:G1ConcMarkStepDurationMillis=5 -XX:G1ConcRSHotCardLimit=16 -XX:G1ConcRefinementServiceIntervalMillis=150";
 
@@ -45,23 +45,36 @@
             symlinks = {
                 mods = pkgs.linkFarmFromDrvs "mods" (
                     builtins.attrValues {
-                        distant-horizons = pkgs.fetchurl {
-                            url = "https://cdn.modrinth.com/data/uCdwusMi/versions/iej5xqn2/DistantHorizons-2.3.6-b-1.21.8-fabric-neoforge.jar";
-                            sha512 = "VsfMKbxXB1JSVzIgzqsB/BpGl8xjYexC+Wy/tBjIexm8znPiUqR9fd4I4SbFbQ35l4iWSENZPD+I/YyJOOfyjw==";
-                        };
                         fabric-api = pkgs.fetchurl {
-                            url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/RMahJx2I/fabric-api-0.136.0+1.21.8.jar";
-                            sha512 = "qGgBysjioUxSoTcFpkdVJcmt4/O+8FORTczl9czeOFQSPFRK7KbPVrdaGR9uNZobm9M7MU8HYveDo6oblLpX6A==";
+                            url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/lxeiLRwe/fabric-api-0.136.0+1.21.10.jar";
+                            sha512 = "sha512-1q1a/rV9xtvhepSJkPyEQfu8E6dIgUpxVmQE2Rk4Tfi9er69pSpYpB62Y3Coa4xPkQtkczsTWUbs1H5TJxMQtQ==";
+                        };
+                        distant-horizons = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/uCdwusMi/versions/9Y10ZuWP/DistantHorizons-2.3.6-b-1.21.10-fabric-neoforge.jar";
+                            sha512 = "sha512-Gxtwt+xikNFSpfn6Py5o6niV9AfFYbVukauj/a3vJ3zSWYeWdhmNZIHcx2oib/GqhXwBrpxBvj6WO1lUYHSh/A==";
+                        };
+                        c2me = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/VSNURh3q/versions/eY3dbqLu/c2me-fabric-mc1.21.10-0.3.5.0.0.jar";
+                            sha512 = "sha512-o0IrdYmak1WqExKGUe0oFf+D/2mMTCKpTqfydcZWr/JHRACFpH3iA1P/VEaVdMhK3JtCjC6WOoCjxmV/uEmCXQ==";
+                        };
+                        lithium = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/gvQqBUqZ/versions/oGKQMdyZ/lithium-fabric-0.20.0+mc1.21.10.jar";
+                            sha512 = "sha512-dVwOD8f284rE2TbMYCPR3Obs/Y1r3CxUTCo8PW0E8NhdtTcioIn6i+cq4y/BJ+h/WUZ5O6botPLCli7TDTM+0g==";
+                        };
+                        ferritecore = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/uXXizFIs/versions/CtMpt7Jr/ferritecore-8.0.0-fabric.jar";
+                            sha512 = "sha512-ExuC0dNm8JZkNb/LOMNi1gTWjs8wwQbTGmJhv8hoyjqCQluz+uuqLl6hfY7tXJKEOBDrLfR5Dy+LHmwb3Jt3RQ==";
+                        };
+                        appleskin = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/EsAfCjCV/versions/8sbiz1lS/appleskin-fabric-mc1.21.9-3.0.7.jar";
+                            sha512 = "sha512-edDQtKCRQM23z3SxzXFVQUfGBki+tIXKZHsUkXThcWYOxWGtMp2li3i13kOZCbGA4oe0s4vwaKz8ogZmEA9FhA==";
+                        };
+                        ping-wheel = pkgs.fetchurl {
+                            url = "https://cdn.modrinth.com/data/QQXAdCzh/versions/2qmGuLd3/Ping-Wheel-1.12.0-fabric-1.21.10.jar";
+                            sha512 = "sha512-0DJVA3Tgz1dxFYXHfoCW0Nd3vvTcwMyZw4QZApeICXRZmDb5YlC6GCrKsk+TsJ7/HTtMIj8+pOzdaOGT/AUrnQ==";
                         };
                     }
                 );
-
-                plugins = pkgs.linkFarmFromDrvs "plugins" [
-                    (pkgs.fetchurl {
-                        url = "https://cdn.modrinth.com/data/IjY7seTG/versions/NudYHdRi/DistantHorizonsSupport-0.11.0-SNAPSHOT.jar";
-                        sha512 = "hD1jT9fa17Vryoq8rhUl/5KG4QW7g2rlHoaWK1geVTh23Oly0ekVSZuq/oGJR9a+AWmLARma9aJQdsdAyjkvSw==";
-                    })
-                ];
             };
         };
     };
