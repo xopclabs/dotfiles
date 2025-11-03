fa4a6f1e998a0520940a701d89cb9ff6cc16413c
xopclabs <b9fyg5ei@duck.com>
2025-11-04 01:29:44 +0300

    Finally fix language widget; add gamescope start

--- modules/desktop/bars/waybar/waybar.nix
+++ modules/desktop/bars/waybar/waybar.nix
@@ -63,6 +63,7 @@ in {
                         drawer = drawer-config;
                         modules = [
                             "custom/power"
+                            "custom/gamescope"
                             "custom/keyboard"
                             "custom/quit"
                             "custom/lock"
@@ -102,8 +103,9 @@ in {
                     };
 
                     "hyprland/language" = {
-                        format = "{short}";
-			keyboard-name = "at-translated-set-2-keyboard";
+                        format-en = "en";
+                        format-ru = "ru";
+                        on-click = "hyprctl switchxkblayout current next";
                         tooltip = false;
                     };
 
@@ -179,6 +181,12 @@ in {
                         on-click = "systemctl reboot";
                     };
 
+                    "custom/gamescope" = {
+                        format = "󰓓";
+                        tooltip = false;
+                        on-click = "start-gamescope-session";
+                    };
+
                     "custom/keyboard" = {
                         format = "󰌌";
                         tooltip = false;
@@ -246,7 +254,7 @@ in {
                 quit-font-size = to-pt base-icon-size;
                 reboot-font-size = to-pt base-icon-size;
                 keyboard-font-size = to-pt base-icon-size;
-                
+                gamescope-font-size = to-pt base-icon-size;
                 # Spacing variables
                 gap = "0.3rem";  # Gap between modules
                 inside-gap = "0.3rem";  # Padding inside modules (controls clickable area)
@@ -297,6 +305,7 @@ in {
             @define-color lock-color @dynamic-yellow;
             @define-color quit-color @dynamic-purple;
             @define-color keyboard-color @dynamic-lightblue;
+            @define-color gamescope-color @dynamic-teal;
 
             /* Reset all styles */
             * {
@@ -375,6 +384,11 @@ in {
                 font-family: "${font-mono}";
                 font-size: ${keyboard-font-size};
             }
+
+            #custom-gamescope {
+                font-family: "${font-mono}";
+                font-size: ${gamescope-font-size};
+            }
             
             /* Text-specific styling */
             #language {
@@ -403,7 +417,8 @@ in {
             #custom-lock,
             #custom-quit,
             #custom-reboot,
-            #custom-keyboard {
+            #custom-keyboard,
+            #custom-gamescope {
                 margin-left: 0px;
                 margin-right: 0px;
                 margin-top: 0px;
@@ -524,6 +539,7 @@ in {
             #custom-lock { color: @lock-color; }
             #custom-quit { color: @quit-color; }
             #custom-keyboard { color: @keyboard-color; }
+            #custom-gamescope { color: @gamescope-color; }
 
             /* Slider-related CSS */
             slider {
