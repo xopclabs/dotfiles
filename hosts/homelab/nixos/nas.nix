06114bff4e6fcf5c6fe3d9627b940f7657221d83
xopclabs <b9fyg5ei@duck.com>
2025-11-30 01:15:06 +0300

    Added samba share

--- hosts/homelab/nixos/nas.nix
+++ hosts/homelab/nixos/nas.nix
@@ -22,7 +22,6 @@
         };
     };
 
-    # NFS server for Proxmox backups
     services.nfs.server = {
         enable = true;
         lockdPort = 4001;
@@ -33,6 +32,33 @@
             /mnt/raid_pool/shared  192.168.254.0/24(rw,sync,no_subtree_check,no_root_squash)
         '';
     };
+
+    services.samba = {
+        enable = true;
+        openFirewall = true;
+        settings = {
+            global = {
+                workgroup = "WORKGROUP";
+                "server string" = "homelab";
+                "netbios name" = "homelab";
+                security = "user";
+                "hosts allow" = "192.168.254. 127.0.0.1 localhost";
+                "hosts deny" = "0.0.0.0/0";
+                "guest account" = "nobody";
+                "map to guest" = "bad user";
+            };
+            shared = {
+                path = "/mnt/raid_pool/shared";
+                browseable = "yes";
+                "read only" = "no";
+                "guest ok" = "no";
+                "valid users" = "@users";
+                "create mask" = "0664";
+                "directory mask" = "0775";
+            };
+        };
+    };
+
     # Open firewall for NFS
     networking.firewall = {
         allowedTCPPorts = [
