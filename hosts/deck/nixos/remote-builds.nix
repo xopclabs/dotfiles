2cdff1401e66c66b405222a3ce2ec4227be7a4e5
xopclabs <b9fyg5ei@duck.com>
2025-12-12 20:49:07 +0300

    Added i686 builds

--- hosts/deck/nixos/remote-builds.nix
+++ hosts/deck/nixos/remote-builds.nix
@@ -2,14 +2,24 @@
 
 {
     nix = {
-        buildMachines = [{
-            hostName = "homelab-builder";
-            system = "x86_64-linux";
-            protocol = "ssh-ng";
-            maxJobs = 4;
-            speedFactor = 2;
-            supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
-        }];
+        buildMachines = [
+            {
+                hostName = "homelab-builder";
+                system = "x86_64-linux";
+                protocol = "ssh-ng";
+                maxJobs = 4;
+                speedFactor = 2;
+                supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+            }
+            {
+                hostName = "homelab-builder";
+                system = "i686-linux";
+                protocol = "ssh-ng";
+                maxJobs = 4;
+                speedFactor = 2;
+                supportedFeatures = [ "nixos-test" "benchmark" "big-parallel" "kvm" ];
+            }
+        ];
         distributedBuilds = true;
         settings.builders-use-substitutes = true;
     };
