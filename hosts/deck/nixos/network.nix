e1808e89513cceb6273af179ca7f02f7ef9082f6
xopclabs <b9fyg5ei@duck.com>
2026-09-08 23:55:38 +0200

    Updated network setup

--- hosts/deck/nixos/network.nix
+++ hosts/deck/nixos/network.nix
@@ -11,6 +11,14 @@
         path = "/etc/NetworkManager/system-connections/home.nmconnection";
         restartUnits = [ "NetworkManager.service" "NetworkManager-dispatcher.service" ];
     };
+    sops.secrets."networkmanager/rented" = {
+        path = "/etc/NetworkManager/system-connections/rented.nmconnection";
+        restartUnits = [ "NetworkManager.service" "NetworkManager-dispatcher.service" ];
+    };
+    sops.secrets."networkmanager/rented2.4g" = {
+        path = "/etc/NetworkManager/system-connections/rented2.4g.nmconnection";
+        restartUnits = [ "NetworkManager.service" "NetworkManager-dispatcher.service" ];
+    };
     sops.secrets."networkmanager/hotspot" = {
         path = "/etc/NetworkManager/system-connections/hotspot.nmconnection";
         restartUnits = [ "NetworkManager.service" "NetworkManager-dispatcher.service" ];
