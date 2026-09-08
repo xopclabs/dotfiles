a80321fd4911418f27341aa44155b92c14f3c460
xopclabs <b9fyg5ei@duck.com>
2026-09-09 00:43:51 +0200

    Fix Traefik ACME DNS propagation checks

--- nixos-modules/selfhost/traefik.nix
+++ nixos-modules/selfhost/traefik.nix
@@ -377,6 +377,7 @@ in
                     storage = "/var/lib/traefik/acme.json";
                     dnsChallenge = {
                         provider = "cloudflare";
+                        resolvers = [ "1.1.1.1:53" "1.0.0.1:53" ];
                     };
                 };
 
